95194e2e14ca3c351f5ba6a6c8a78de00544fa71
Francieli Silva Abreu <fsilvaab@everis.com>
2020-11-17 12:01:11 -0300

    .

--- src/main/java/br/hoteleveris/app/controller/FaturaController.java
+++ src/main/java/br/hoteleveris/app/controller/FaturaController.java
@@ -2,7 +2,7 @@ package br.hoteleveris.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
